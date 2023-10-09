9ad2623644d374335026c7412978e667e7788842
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2023-10-10 08:05:31 +1100

    Remove unimplemented algorithms

--- src/gui.gd
+++ src/gui.gd
@@ -2,6 +2,12 @@ extends Control
 
 signal generate_button_pressed
 
+@onready var generate_button: Button = $CenterContainer/VBoxContainer/Generate
 
-func _on_generate_pressed():
+
+func _on_generate_pressed() -> void:
 	generate_button_pressed.emit()
+
+
+func _on_algorithm_options_item_selected(_index) -> void:
+	generate_button.disabled = false
