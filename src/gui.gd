4b921b278d929e60496fc93e09671ba4e7887ed1
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2023-10-11 08:16:14 +1100

    Update generate button to disabled after click

--- src/gui.gd
+++ src/gui.gd
@@ -7,6 +7,7 @@ signal generate_button_pressed
 
 func _on_generate_pressed() -> void:
 	generate_button_pressed.emit()
+	generate_button.disabled = true
 
 
 func _on_algorithm_options_item_selected(_index) -> void:
