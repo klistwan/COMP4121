d396138b6a81c508cedca108650885c192fbadd6
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2023-11-19 10:17:30 +1100

    Rename algorithms to match paper

--- src/gui.gd
+++ src/gui.gd
@@ -4,17 +4,16 @@ signal generate_button_pressed(algorithm_path, params)
 
 var selected_item_index: int = -1
 var algorithms := {
-	0: "res://src/map/algorithms/grid_based_room_placement.gd",
 	1: "res://src/map/algorithms/random_room_placement.gd",
-	2: "res://src/map/algorithms/room_accretion.gd",
-	3: "res://src/map/algorithms/random_walk.gd",
-	4: "res://src/map/algorithms/cellular_automata.gd",
-	5: "res://src/map/algorithms/cellular_automata.gd",
-	6: "res://src/map/algorithms/cellular_automata.gd",
-	7: "res://src/map/algorithms/fast_noise.gd",
-	8: "res://src/map/algorithms/fast_noise.gd",
-	9: "res://src/map/algorithms/voronoi_relaxation.gd",
-	10: "res://src/map/algorithms/voronoi_relaxation.gd",
+	2: "res://src/map/algorithms/grid_based_room_placement.gd",
+	7: "res://src/map/algorithms/random_walk.gd",
+	8: "res://src/map/algorithms/cellular_automata.gd",
+	9: "res://src/map/algorithms/cellular_automata.gd",
+	10: "res://src/map/algorithms/cellular_automata.gd",
+	12: "res://src/map/algorithms/fast_noise.gd",
+	13: "res://src/map/algorithms/fast_noise.gd",
+	14: "res://src/map/algorithms/voronoi_relaxation.gd",
+	15: "res://src/map/algorithms/voronoi_relaxation.gd",
 }
 
 @onready var generate_button: Button = $CenterContainer/VBoxContainer/Generate
@@ -24,22 +23,22 @@ func _on_generate_pressed() -> void:
 	generate_button.disabled = true
 	var params := {}
 	match selected_item_index:
-		4:  # Anneal
+		8:  # Anneal
 			params["born"] = [4, 6, 7, 8]
 			params["survive"] = [3, 5, 6, 7, 8]
-		5:  # Assimilation
+		9:  # Assimilation
 			params["born"] = [4, 5, 6, 7]
 			params["survive"] = [3, 4, 5]
-		6:  # Diamoeba
+		10:  # Diamoeba
 			params["born"] = [5, 6, 7, 8]
 			params["survive"] = [3, 5, 6, 7, 8]
-		7:
+		12:
 			params["noise_type"] = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
-		8:
+		13:
 			params["noise_type"] = FastNoiseLite.TYPE_PERLIN
-		9:
+		14:
 			params["iteration_count"] = 1
-		10:
+		15:
 			params["iteration_count"] = 5
 	generate_button_pressed.emit(algorithms[selected_item_index], params)
 
